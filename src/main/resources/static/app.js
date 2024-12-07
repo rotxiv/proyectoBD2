9d074dace46683ad78e495720347ef0562d41fcd
rotxiv <rotxiv0@gmail.com>
2024-12-06 20:04:14 -0400

    siguiente actualización

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -22,64 +22,11 @@ const graficaVeintiuno = document.getElementById('graficaVeintiuno');
 const graficaVeintidos = document.getElementById('graficaVeintidos');
 const graficaVeintitres = document.getElementById('graficaVeintitres');
 
-
-
-/*function functionChart(grafica, localidad, inscritos) {
-    const myChart = new Chart(grafica, {
-        type: 'bar',
-        data: {
-            labels: localidad,
-            datasets: [{
-                label: 'Edad',
-                data: inscritos,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                    'rgba(255, 205, 86, 0.2)',
-                    'rgba(153, 102, 255, 0.2)'
-                ],
-                borderColor: [
-                    'rgb(255, 99, 132)',
-                    'rgb(255, 159, 64)',
-                    'rgb(255, 205, 86)',
-                    'rgb(153, 102, 255)'
-                ],
-                borderWidth: 1
-            }],
-        },
-        options: {
-            responsive: true,
-            maintainAspectRatio: true,
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            },
-        },
-    });
-};
-
-const cargarGraficoUno = async() => {
-    try {
-        const respuesta = await fetch(`${URL_API}/GraficaUno/2020-1`);
-        let id = [], localidad = [], inscritos = [];
-        if (respuesta.status === 200) {
-            const datos = await respuesta.json();
-            datos.forEach(element => {
-               id.push(element.id);
-               localidad.push(element.localidad);
-               inscritos.push(element.inscritos);
-            });
-            functionChart(grafica,localidad,inscritos);
-        }
-
-    }catch(error){
-		console.log(error);
-	}
-};*/
  
 graficaUno.addEventListener('click', () => {
-	cargarGraficoUno();
+	windows.alert("soy un mensaje de alerta");
+    opcionesUno();
+	//cargarGraficoUno();
 });
 
 graficaDos.addEventListener('click', () => {
--- src/main/resources/static/funciones.js
+++ src/main/resources/static/funciones.js
@@ -1,5 +1,10 @@
 const URL_API = "http://localhost:8080/API/Academico";
 const grafica = document.getElementById('grafica');
+const periodo = document.getElementById('periodos');
+const localidad = document.getElementById('localidades');
+const facultad = document.getElementById('facultades');
+const modalidad = document.getElementById('modalidades');
+
 
 function functionChart(grafica, localidad, inscritos) {
     const myChart = new Chart(grafica, {
@@ -36,9 +41,35 @@ function functionChart(grafica, localidad, inscritos) {
     });
 };
 
+
+function opcionesUno() {
+    alert("¡Hola..., Soy un mensaje de alerta...!");
+    localidad.style.display = "none";
+    modalidad.style.display = "none";
+    facultad.style.display = "none";
+    periodo.style.display = "inline";
+};
 const cargarGraficoUno = async() => {
     try {
-        const respuesta = await fetch(`${URL_API}/GraficaUno/2020-1`);
+        const respuesta = await fetch(`${URL_API}/GraficaUno/2019-1`);
+        let id = [], localidad = [], inscritos = [];
+        if (respuesta.status === 200) {
+            const datos = await respuesta.json();
+            datos.forEach(element => {
+               id.push(element.id);
+               localidad.push(element.localidad);
+               inscritos.push(element.inscritos);
+            });
+            functionChart(grafica,localidad,inscritos);
+        }
+    }catch(error){
+		console.log(error);
+	}
+};
+
+const cargarGraficoDos = async() => {
+    try {
+        const respuesta = await fetch(`${URL_API}/Graficados/param`);
         let id = [], localidad = [], inscritos = [];
         if (respuesta.status === 200) {
             const datos = await respuesta.json();
@@ -54,3 +85,87 @@ const cargarGraficoUno = async() => {
 		console.log(error);
 	}
 };
+
+const cargarGraficoTres = async () => {
+    
+}
+
+const cargarGraficoCuatro = async () => {
+    
+}
+
+const cargarGraficoCinco = async () => {
+    
+}
+
+const cargarGraficoSeis = async () => {
+    
+}
+
+const cargarGraficoSiete = async () => {
+    
+}
+
+const cargarGraficoOcho = async () => {
+    
+}
+
+const cargarGraficoNueve = async () => {
+    
+}
+
+const cargarGraficoDiez = async () => {
+    
+}
+
+const cargarGraficoOnce = async () => {
+    
+}
+
+const cargarGraficoDoce = async () => {
+    
+}
+
+const cargarGraficoTrece = async () => {
+    
+}
+
+const cargarGraficoCatorce = async () => {
+    
+}
+
+const cargarGraficoQuince = async () => {
+    
+}
+
+const cargarGraficoDieciseis = async () => {
+    
+}
+
+const cargarGraficoDiecisiete = async () => {
+    
+}
+
+const cargarGraficoDieciocho = async () => {
+    
+}
+
+const cargarGraficoDiecinueve = async () => {
+    
+}
+
+const cargarGraficoVeinte = async () => {
+    
+}
+
+const cargarGraficoVeintiuno = async () => {
+    
+}
+
+const cargarGraficoVeintidos = async () => {
+    
+}
+
+const cargarGraficoVeintitres = async () => {
+    
+}
